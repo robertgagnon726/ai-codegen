b1b6fec17975e37357dbc0dc343e08a79d21c80f
Bobby Gagnon <robertgagnon726@gmail.com>
2024-09-30 22:54:36 -0400

    added CI pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,16 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     coverage: {
-      provider: 'istanbul', 
+      exclude: ['internals', 'dist'],
+      provider: 'istanbul', // Ensures Istanbul is used for coverage
+      reporter: ['text', 'lcov'],
+      all: true, // Include all files, even those not tested
+      thresholds: {
+        statements: 90,
+        branches: 70,
+        functions: 90,
+        lines: 90,
+      }
     },
   },
 });
